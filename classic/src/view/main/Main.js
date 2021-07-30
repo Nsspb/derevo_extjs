b18e53427c069563403d52cc46325294c8c7c746
Konstantin <whitehorse.spb@gmail.com>
2021-07-30 18:57:25 +0300

    init commit

---
+++ classic/src/view/main/Main.js
@@ -0,0 +1,11 @@
+Ext.define('MyAppName.view.main.Main', {
+    extend: 'Ext.container.Container',
+  
+    xtype: 'app-main',
+    items : [
+        {
+            xtype: 'k1'
+        }
+    ]
+   
+});
